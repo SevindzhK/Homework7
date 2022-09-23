b594c471401f6dd0e4295ebae2cd808de1e17006
SevindzhK <sevinge0709@gmail.com>
2022-09-23 13:24:09 +0300

    Added Ex47

---
+++ Ex47/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int rows = new Random().Next(3, 6);
+int columns = new Random().Next(3, 6);
+double[,] array = new double[rows, columns];
+
+Random x = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Convert.ToDouble(x.Next(-100, 100) / 10.0);
+        Console.Write($"{array[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
